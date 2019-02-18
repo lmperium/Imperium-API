c7078fc7b9cf436e9c4749225348e2ef6fb26166
krekel <luisg.diazpr@gmail.com>
2019-02-18 13:45:54 -0400

    Fixed test cases for analyst.

--- analyst_test.py
+++ analyst_test.py
@@ -14,28 +14,30 @@ class DatabaseTest(unittest.TestCase):
             db.drop_all()
             db.create_all()
 
-        self.analyst_info = {
+        self.analyst_info = json.dumps({
             'first_name': 'John',
             'last_name': 'Doe',
             'username': 'TestUser123',
             'password': 'EWesWQ211',
-        }
+            'email': 'test@mail.com'
+        })
 
     def test_analyst_registration(self):
         """Test API analyst registration."""
         response = self.client().post('api/analysts', data=self.analyst_info)
         self.assertEqual(response.status_code, 201)
-        self.assertIn('Analyst Registered', str(response.data))
+        self.assertIn('John', str(response.data))
 
         # Test registering the same user.
-        response = self.clien().post('api/analysts', data=self.analyst_info)
-        self.assertEqual(response.status_code, 200)
-        self.asserIn('Analyst already Registered', str(response.data))
+        response = self.client().post('api/analysts', data=self.analyst_info)
+        self.assertEqual(response.status_code, 409)
+        self.assertIn('Analyst already registered', str(response.data))
 
     def test_get_all_analysts(self):
         """Test API get all analysts."""
         response = self.client().get('api/analysts')
-        self.assertIn('No analysts registered', str(response.data))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('analysts', str(response.data))
 
         response = self.client().post('api/analysts', data=self.analyst_info)
         self.assertEqual(response.status_code, 201)
@@ -49,16 +51,19 @@ class DatabaseTest(unittest.TestCase):
 
         # Specified analyst does not exist.
         response = self.client().get('api/analysts/3')
+        self.assertEqual(response.status_code, 404)
         self.assertIn('Resource not found', str(response.data))
 
         # Add analyst.
         response = self.client().post('api/analysts', data=self.analyst_info)
         self.assertEqual(response.status_code, 201)
 
-        res_json = json.loads(response.data.encode('utf-8').replace("'", "\'"))
+        res_json = json.loads(response.data.decode('utf-8').replace("'", "\'"))
+
+        analyst_id = res_json['analyst_id']
 
         # Test for specified analyst.
-        response = self.client().get(f'api/analayst/{res_json["analyst_id"]}')
+        response = self.client().get(f'api/analysts/{analyst_id}')
         self.assertEqual(response.status_code, 200)
         self.assertIn('John', str(response.data))
 
