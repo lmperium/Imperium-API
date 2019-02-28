a85445a98f9063974928c7d9f1b413c84a785205
krekel <luisg.diazpr@gmail.com>
2019-02-28 12:40:31 -0400

    Added serializationa and deserialization methods to worker model. Minor changes to worker model.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,21 @@
 from app import db
+from sqlalchemy import text
 from sqlalchemy.dialects.postgresql import JSON
 from werkzeug.security import generate_password_hash, check_password_hash
+
+
+def to_collection_dict(response) -> dict:
+    """
+    Convert a collection of items into a Python dictionary object.
+    :param response: SQLAlchemy query.all() Tuple
+    :return: dictionary object data containing the list of items
+    """
+
+    data = {
+        'objects': [item.to_dict() for item in response]
+    }
+
+    return data
 
 
 class Analyst(db.Model):
@@ -13,7 +28,7 @@ class Analyst(db.Model):
     email = db.Column(db.String(60), unique=True, index=True, nullable=False)
     username = db.Column(db.String(100), unique=True, index=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    registered_date = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False)
+    registered_date = db.Column(db.DateTime, server_default=text('now()'), nullable=False)
 
     def set_password(self, password):
         self.password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=15)
@@ -42,20 +57,6 @@ class Analyst(db.Model):
         if new_user and 'password' in data:
             self.set_password(data['password'])
 
-    @staticmethod
-    def to_collection_dict(response) -> dict:
-        """
-        Convert a collection of items into a Python dictionary object.
-        :param response: SQLAlchemy query.all() Tuple
-        :return: dictionary object data containing the list of items
-        """
-
-        data = {
-            'analysts': [item.to_dict() for item in response]
-        }
-
-        return data
-
 
 class Job(db.Model):
     __tablename__ = 'job'
@@ -64,7 +65,7 @@ class Job(db.Model):
     name = db.Column(db.String(250), nullable=False)
     target = db.Column(db.String(250), nullable=False)
     description = db.Column(JSON, nullable=False)
-    start_time = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False)
+    start_time = db.Column(db.DateTime, server_default=text('now()'), nullable=False)
     end_time = db.Column(db.DateTime, nullable=True)
     status = db.Column(db.String(50), nullable=False)
     analyst_id = db.Column(db.Integer, db.ForeignKey('analyst.analyst_id'))
@@ -75,10 +76,36 @@ class Worker(db.Model):
     __tablename__ = 'worker'
 
     worker_id = db.Column(db.Integer, primary_key=True)
-    registered_date = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False)
+    hostname = db.Column(db.String(255), unique=True, index=True, nullable=False)
     status = db.Column(db.String(50), nullable=False)
-    last_seen = db.Column(db.DateTime, nullable=False)
+    target_queue = db.Column(db.String(255), nullable=False, index=True)
+    registered_date = db.Column(db.DateTime, server_default=text('now()'), nullable=False)
+    last_seen = db.Column(db.DateTime, server_default=text('now()'), nullable=False)
     startup_info = db.Column(JSON)
+
+    def from_dict(self, data):
+        """ Convert json data to a Worker object and initialize fields."""
+        setattr(self, 'target_queue', self._generate_queue_name(data))
+        setattr(self, 'hostname', data['hostname'])
+        setattr(self, 'status', 'active')
+        setattr(self, 'startup_info', data['startup_info'])
+
+    def to_dict(self):
+        data = {
+            'hostname': self.hostname,
+            'registered_date': self.registered_date,
+            'status': self.status,
+            'target_queue': self.target_queue,
+            'last_seen': self.last_seen,
+            'startup_info': self.startup_info
+        }
+
+        return data
+
+    @staticmethod
+    def _generate_queue_name(data):
+        queue_name = 'imp.wk.'
+        return queue_name + data['hostname']
 
 
 class Command(db.Model):
