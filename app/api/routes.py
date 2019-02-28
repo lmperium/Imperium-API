42a76f9e4d7ab8410657f07597d89b543af67d20
krekel <luisg.diazpr@gmail.com>
2019-02-28 12:35:43 -0400

    Moved all endpoints into one file and added worker endpoints.

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,11 +1,15 @@
+import logging
+
 from app import db
 from app.api.errors import error_response
-from app.models import Analyst
+from app.models import Analyst, Job, Worker, to_collection_dict
 from flask import Blueprint
 from flask import jsonify, request
 from flask_jwt_extended import jwt_required
 
 bp = Blueprint('api', __name__)
+
+# Analyst routes
 
 
 @bp.route('/analysts/<int:analyst_id>', methods=['GET'])
@@ -24,7 +28,7 @@ def get_analyst(analyst_id: int):
 def get_analysts():
     response = Analyst.query.all()
 
-    return jsonify(Analyst.to_collection_dict(response))
+    return jsonify(to_collection_dict(response))
 
 
 @bp.route('/analysts', methods=['POST'])
@@ -69,3 +73,69 @@ def _user_exists(data):
         return True
 
     return False
+
+
+# Worker routes
+
+
+@bp.route('/workers', methods=['GET'])
+@jwt_required
+def get_workers():
+
+    response = Worker.query.all()
+
+    return jsonify(to_collection_dict(response))
+
+
+@bp.route('/workers', methods=['POST'])
+def register_worker():
+    data = request.get_json(force=True)
+
+    if Worker.query.filter_by(hostname=data['hostname']).first():
+        return error_response(409, 'Worker already registered.')
+
+    worker = Worker()
+    worker.from_dict(data)
+
+    db.session.add(worker)
+    db.session.commit()
+
+    response = jsonify(worker.to_dict())
+    response.status_code = 201
+
+    return response
+
+
+@bp.route('/workers/<int:job_id>', methods=['GET'])
+@jwt_required
+def get_worker(worker_id: int):
+    pass
+
+
+# Job routes
+
+
+@bp.route('/jobs', methods=['POST'])
+@jwt_required
+def create_job():
+    data = request.get_json(force=True) or {}
+
+    pass
+
+
+@bp.route('/jobs/<int:job_id>', methods=['GET'])
+@jwt_required
+def get_job(job_id: int):
+    pass
+
+
+@bp.route('/jobs/results', methods=['PUT'])
+@jwt_required
+def upload_results():
+    pass
+
+
+@bp.route('/jobs/results/<int:job_id>', methods=['GET'])
+@jwt_required
+def get_results():
+    pass
